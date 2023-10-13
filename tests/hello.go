b9041bf9e830296036251efb1e7759d6e1b04217
Mert Öztürk <mert.ozturk@teleskop.app>
2023-10-13 20:48:57 +0300

    added switch case

--- tests/hello.go
+++ tests/hello.go
@@ -3,13 +3,44 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+/*
+	func Hello(name string, language string) string {
+		if name == "" {
+			name = "World"
+		}
+		if language == spanish {
+			return spanishHelloPrefix + name
+		}
+		if language == french {
+			return frenchHelloPrefix + name
+		}
+		return englishHelloPrefix + name
+	}
+*/
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("Mert", "French"))
 }
