f8c0dea6b4a78e1e3ae777e1ceb522ef3fd33425
Mert Öztürk <mert.ozturk@teleskop.app>
2023-10-14 13:51:30 +0300

    integers example added

---
+++ tests/integers/adder_test.go
@@ -0,0 +1,24 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func Add(x, y int) int { // x int y int -> x, y int
+	return x + y
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+}
