2c15595b27d417d9faeb87d2f9ab9640608e3b60
Mert Öztürk <mert.ozturk@teleskop.app>
2023-10-21 16:19:55 +0300

    methods | struct examples added

--- tests/structs-methods-interfaces/v2/perimeter.go
+++ tests/structs-methods-interfaces/v2/perimeter.go
@@ -1,8 +1,26 @@
 package v2
 
+import "math"
+
 type Rectangle struct {
 	height float64
 	width  float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+// methodlar structlarla birlikte kullanılır. Kullanim yapisi fonksiyonlara cok benze
+// func (receiverName ReceiverType) MethodName(args)
+// receiverName: methodun bagli oldugu structun ismi ve structerin bas harfi kucuk olmasi bir kuraldir
+
+func (r Rectangle) Area() float64 {
+	return r.height * r.width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- tests/structs-methods-interfaces/v2/shapes_test.go
+++ tests/structs-methods-interfaces/v2/shapes_test.go
@@ -13,11 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{height: 10.0, width: 10.0}
-	got := Area(rectangle)
-	want := 100.0
+	t.Run("Rectangle Area", func(t *testing.T) {
+		rectangle := Rectangle{height: 12.0, width: 6.0}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("Circle Area", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+
+		}
+	})
 }
